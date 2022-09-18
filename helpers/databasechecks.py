7735502d0d180704243b97752e6dad3b110cc5f6
KevinBex <kevinbex1@gmail.com>
2022-09-18 14:42:24 +0200

    checks db

--- helpers/databasechecks.py
+++ helpers/databasechecks.py
@@ -1,5 +1,5 @@
+from helpers import db_manager
 from db_manager import startsql as sql
-
 
 async def is_blacklisted(user_id: int) -> bool:
     """
@@ -18,13 +18,9 @@ async def add_user_to_blacklist(user_id: int) -> int:
 
     :param user_id: The ID of the user that should be added into the blacklist.
     """
-    connection = sqlite3.connect("database/database.db")
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO blacklist(user_id) VALUES (?)", (user_id,))
-    connection.commit()
-    rows = cursor.execute("SELECT COUNT(*) FROM blacklist").fetchone()[0]
-    connection.close()
-    return rows
+    await sql.execute("INSERT INTO blacklist(user_id) VALUES (?)", (user_id,))
+    rows = await sql.fetchone("SELECT COUNT(*) FROM blacklist")
+    return rows[0]
 
 
 async def remove_user_from_blacklist(user_id: int) -> int:
@@ -33,13 +29,9 @@ async def remove_user_from_blacklist(user_id: int) -> int:
 
     :param user_id: The ID of the user that should be removed from the blacklist.
     """
-    connection = sqlite3.connect("database/database.db")
-    cursor = connection.cursor()
-    cursor.execute("DELETE FROM blacklist WHERE user_id=?", (user_id,))
-    connection.commit()
-    rows = cursor.execute("SELECT COUNT(*) FROM blacklist").fetchone()[0]
-    connection.close()
-    return rows
+    await sql.execute("DELETE FROM blacklist WHERE user_id=?", (user_id,))
+    rows = await sql.fetchone("SELECT COUNT(*) FROM blacklist")
+    return rows[0]
 
 
 async def add_warn(user_id: int, server_id: int, moderator_id: int, reason: str) -> int:
@@ -49,19 +41,14 @@ async def add_warn(user_id: int, server_id: int, moderator_id: int, reason: str)
     :param user_id: The ID of the user that should be warned.
     :param reason: The reason why the user should be warned.
     """
-    connection = sqlite3.connect("database/database.db")
-    cursor = connection.cursor()
     # Get the last `id`
-    rows = cursor.execute("SELECT id FROM warns WHERE user_id=? AND server_id=? ORDER BY id DESC LIMIT 1",
-                          (user_id, server_id,)).fetchone()
+    rows = await sql.fetchone("SELECT id FROM warns WHERE user_id=? AND server_id=? ORDER BY id DESC LIMIT 1",
+                          (user_id, server_id,))
     warn_id = rows[0] + 1 if rows is not None else 1
-    cursor.execute("INSERT INTO warns(id, user_id, server_id, moderator_id, reason) VALUES (?, ?, ?, ?, ?)",
+    await sql.execute("INSERT INTO warns(id, user_id, server_id, moderator_id, reason) VALUES (?, ?, ?, ?, ?)",
                    (warn_id, user_id, server_id, moderator_id, reason,))
-    connection.commit()
-    rows = \
-    cursor.execute("SELECT COUNT(*) FROM warns WHERE user_id=? AND server_id=?", (user_id, server_id,)).fetchone()[0]
-    connection.close()
-    return rows
+    rows = await sql.fetchone("SELECT COUNT(*) FROM warns WHERE user_id=? AND server_id=?", (user_id, server_id,))
+    return rows[0]
 
 
 async def remove_warn(warn_id: int, user_id: int, server_id: int) -> int:
@@ -72,14 +59,9 @@ async def remove_warn(warn_id: int, user_id: int, server_id: int) -> int:
     :param user_id: The ID of the user that was warned.
     :param server_id: The ID of the server where the user has been warned
     """
-    connection = sqlite3.connect("database/database.db")
-    cursor = connection.cursor()
-    cursor.execute("DELETE FROM warns WHERE id=? AND user_id=? AND server_id=?", (warn_id, user_id, server_id,))
-    connection.commit()
-    rows = \
-    cursor.execute("SELECT COUNT(*) FROM warns WHERE user_id=? AND server_id=?", (user_id, server_id,)).fetchone()[0]
-    connection.close()
-    return rows
+    await sql.execute("DELETE FROM warns WHERE id=? AND user_id=? AND server_id=?", (warn_id, user_id, server_id,))
+    rows = await sql.fetchone("SELECT COUNT(*) FROM warns WHERE user_id=? AND server_id=?", (user_id, server_id,))
+    return rows[0]
 
 
 async def get_warnings(user_id: int, server_id: int) -> list:
@@ -90,11 +72,6 @@ async def get_warnings(user_id: int, server_id: int) -> list:
     :param server_id: The ID of the server that should be checked.
     :return: A list of all the warnings of the user.
     """
-    connection = sqlite3.connect("database/database.db")
-    cursor = connection.cursor()
-    cursor.execute(
-        "SELECT user_id, server_id, moderator_id, reason, strftime('%s', created_at), id FROM warns WHERE user_id=? AND server_id=?",
+    result = await sql.fetchall("SELECT user_id, server_id, moderator_id, reason, strftime('%s', created_at), id FROM warns WHERE user_id=? AND server_id=?",
         (user_id, server_id,))
-    result = cursor.fetchall()
-    connection.close()
     return result
