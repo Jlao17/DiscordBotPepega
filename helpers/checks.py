34acb663b3a5a0a6e549911aa9d6d2094287509a
KevinBex <kevinbex1@gmail.com>
2022-09-18 14:28:05 +0200

    checks db

--- helpers/checks.py
+++ helpers/checks.py
@@ -39,7 +39,7 @@ def not_blacklisted() -> Callable[[T], T]:
     """
 
     async def predicate(context: commands.Context) -> bool:
-        if db_manager.is_blacklisted(context.author.id):
+        if databasechecks.is_blacklisted(context.author.id):
             raise UserBlacklisted
         return True
 
