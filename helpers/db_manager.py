885ae6859710a8dc43928bb552453eda2cfcf2e0
KevinBex <kevinbex1@gmail.com>
2022-09-18 15:15:49 +0200

    revert

--- helpers/db_manager.py
+++ helpers/db_manager.py
@@ -1,11 +1,3 @@
-""""
-Copyright © Krypton 2022 - https://github.com/kkrypt0nn (https://krypton.ninja)
-Description:
-This is a template to create your own discord bot in python.
-
-Version: 5.1
-"""
-
 import sqlite3
 
 def is_blacklisted(user_id: int) -> bool:
