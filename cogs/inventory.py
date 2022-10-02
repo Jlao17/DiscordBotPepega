b486323fb132042f1c21938195e0f2e123d87017
Jlao17 <jasonlao1707@gmail.com>
2022-10-02 13:17:27 +0200

    Create inventory.py

---
+++ cogs/inventory.py
@@ -0,0 +1,29 @@
+from discord.ext import commands
+from helpers import db_connect
+from helpers import checks
+
+
+# Here we name the cog and create a new class for the cog.
+class Inventory(commands.Cog, name="inventory"):
+    def __init__(self, bot):
+        self.bot = bot
+        self.sql = db_connect.startsql
+
+
+    # Here you can just add your own commands, you'll always need to provide "self" as first parameter.
+    @commands.hybrid_command(
+        name="inventory",
+        description="Open your inventory.",
+    )
+    async def inventory(self, ctx, discordid=None):
+        if discordid is None:
+            discordid = ctx.message.author.id
+
+        output = self.sql.fetchone("SELECT * FROM user WHERE userid = %s", discordid)
+        print(output)
+        await ctx.send(output)
+
+
+# And then we finally add the cog to the bot so that it can load, unload, reload and use it's content.
+async def setup(bot):
+    await bot.add_cog(Inventory(bot))
