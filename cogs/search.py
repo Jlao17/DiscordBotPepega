efd991808ad804ae5a13090ff0bc5e527e697c73
Jlao17 <jasonlao1707@gmail.com>
2022-12-29 17:09:24 +0100

    Update search.py

--- cogs/search.py
+++ cogs/search.py
@@ -23,14 +23,19 @@ class Search(commands.Cog, name="search"):
         """https://api-docs.igdb.com/#about"""
         byte_array = self.wrapper.api_request(
             'games',
-            'fields name; limit 10; where category = 0 & platforms = 6; search "{}";'.format(args)
+            'fields name, alternative_names.*; limit 10; where category = (0,8,9); search "{}";'.format(args)
         )
         data = json.loads(byte_array)
+        # Mimic browser API request
+        g2a_headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0"
+        }
+        print(json.dumps(data, indent=4))
         # Search results 0
         if len(data) < 1:
             await ctx.send("I've found no game")
         # Search results more than 1
-        elif len(data) > 1:
+        elif len(data) >= 1:
             x = 1
             game_list = []
             for game in data:
@@ -44,33 +49,68 @@ class Search(commands.Cog, name="search"):
 
             def get_game(args):
                 game_appid = None
-                print(args)
+                prices_embed = discord.Embed(
+                    title="Results",
+                    description="Test",
+                    color=0x9C84EF
+                )
+                print("args: " + args)
                 for app in self.steam_apps["applist"]["apps"]:
                     if app["name"].lower() == args.lower():
                         print("found it")
                         game_appid = str(app["appid"])
+                        print("appid:" + game_appid)
                         break
                 if game_appid is None:
                     print("game not found")
                     return
 
-                game_json = requests.get("http://store.steampowered.com/api/appdetails?appids=" + game_appid).json()
-                game_data = game_json[game_appid]["data"]
-                price = game_data["price_overview"]
-                price_currency = price["currency"]
-                price_final = price["final"]
-                price_discount = price["discount_percent"]
+                game_json_steam = requests.get(
+                    "http://store.steampowered.com/api/appdetails?appids=" + game_appid
+                ).json()
+                game_data = game_json_steam[game_appid]["data"]
+                game_name = game_data["name"]
+                game_json_g2a = requests.get(
+                    "https://www.g2a.com/search/api/v2/products?itemsPerPage=4&"
+                    "include[0]=filters&currency=EUR&isWholesale=false&category=1&"
+                    "f[regions][0]=8355&f[device][0]=1118&phrase=" + game_name, headers=g2a_headers
+                ).json()
+                for g2a_app in game_json_g2a["data"]["items"]:
+                    print(g2a_app)
+                    g2a_app_url = "https://www.g2a.com" + g2a_app["href"]
+                    g2a_app_price = g2a_app["price"] + g2a_app["currency"]
+                    g2a_app_name = g2a_app["name"]
+                    embed_name = g2a_app_name + " - " + g2a_app_price
+                    prices_embed.add_field(
+                        name="G2A - {price}".format(price=g2a_app_price),
+                        value="[{name}]({url})".format(name=embed_name, url=g2a_app_url)
+                    )
+                print("out loop")
 
-                price_total = game_data["name"] + ": " + price_currency + str(price_final / 100) + \
-                              " " + str(price_discount) + "% discount"
-                return price_total
+                if "price_overview" not in game_data:
+                    print("key not found")
+                    price_total = game_name + ": Free"
+                else:
+                    price = game_data["price_overview"]
+                    price_currency = price["currency"]
+                    print(price_currency)
+                    price_final = price["final"]
+                    print(price_final)
+                    price_total = game_name + ": " + price_currency + str(price_final / 100)
+                # price_discount = price["discount_percent"]
+                prices_embed.add_field(name="Steam - " + price_total, value="test")
+                print("reached steam end")
+                return prices_embed
 
             async def print_game(interaction, choice):
-                check_game = get_game(choice["name"])
-                if check_game is None:
+                check_name = get_game(choice["name"])
+                if check_name is None:
+                    for alt_name in choice["alternative_names"]:
+                        check_name = get_game(alt_name["name"])
+                if check_name is None:
                     await interaction.response.send_message("Game could not be found on Steam.")
                 else:
-                    await interaction.response.send_message(check_game)
+                    await interaction.response.send_message(embed=check_name)
 
             async def callback(interaction):
                 if select.values[0] == "1":
