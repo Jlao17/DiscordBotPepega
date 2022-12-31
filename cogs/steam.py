f66fbc93df18bdf98266dde70503deffff8eb9eb
kevin <kevinbex1@gmail.com>
2022-12-31 17:15:41 +0100

    steamfilldb

--- cogs/steam.py
+++ cogs/steam.py
@@ -1,5 +1,5 @@
 import platform
-
+import traceback
 import discord
 import requests
 import json
@@ -10,7 +10,7 @@ from helpers.db_connect import startsql as sql
 import re
 
 
-class steam(commands.Cog, name="steam"):
+class Steam(commands.Cog, name="steam"):
     def __init__(self, bot):
         self.bot = bot
 
@@ -26,20 +26,52 @@ class steam(commands.Cog, name="steam"):
             await ctx.send("{} is not in {}".format(args, stri))
 
     @commands.hybrid_command(
-        name="test",
+        name="fillsteamdb",
         description="compare",
     )
-    async def test(self, ctx, game_name, vendor_name):
-        trigger_words = ["remake", "remade", "remaster", "remastered"]
-        # Check eerst of de trigger woord al in de searched naam zit en dan ook in vendor_naam
-        for word in trigger_words:
-            print(word)
-            if re.search(r'\b' + word + r'\b', game_name):
-                for word2 in trigger_words:
-                    if re.search(r'\b' + word2 + r'\b', vendor_name):
-                        return await ctx.send("FOUND ON BOTH ENDS")
-                return await ctx.send("FOUND FIRST ONLY")
-        return await ctx.send("FOUND NOPES")
+    async def fillsteamdb(self, ctx):
+        error = []
+        await ctx.send("Filling it up! Will notify any updates")
+        steam_apps = requests.get("https://api.steampowered.com/ISteamApps/GetAppList/v2").json()
+        count = 0
+        for app in steam_apps["applist"]["apps"]:
+            count += 1
+            # if normalized_text(app["name"].lower()) == normalized_text(args.lower()):
+            game_appid = str(app["appid"])
+            game_json_steam = requests.get(
+                "http://store.steampowered.com/api/appdetails?appids=" + game_appid + "&l=english&&currency=3"
+            ).json()
+            print(game_json_steam)
+            try:
+                game_data = game_json_steam[game_appid]["data"]
+                if game_json_steam is not None:
+                    if game_data["type"] == "game":
+                        game_name = app["name"]
+                        game_thumbnail = game_data["header_image"]
+                        if game_data["is_free"] == "true":
+                            game_price_initial = 0
+                            game_price_final = 0
+                            game_price_discount = 0
+                        else:
+                            game_price_initial = game_data["price_overview"]["initial"]
+                            game_price_final = game_data["price_overview"]["final"]
+                            game_price_discount = game_data["price_overview"]["discount_percent"]
+                        game_type = game_data["type"]
+                        sql.execute("INSERT INTO "
+                                    "steam_games (id, name, steam_id, thumbnail, price_initial, price_final, discount, type) "
+                                    "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+                                    (str(count), game_name, str(game_appid), game_thumbnail, str(game_price_initial),
+                                     str(game_price_final),
+                                     str(game_price_discount), game_type))
+            except:
+                error.append(game_appid)
+                traceback.print_exc()
+                if count % 5 == 0:
+                    await ctx.send("Failed inserting: {}".format(error))
+                    error.clear()
+
+        # await ctx.send("<@186120333726580736> <@160805480644476928> done")
+
 
 async def setup(bot):
-    await bot.add_cog(steam(bot))
+    await bot.add_cog(Steam(bot))
