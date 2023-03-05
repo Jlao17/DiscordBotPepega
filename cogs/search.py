d9cb82a0751da87254007e72b65e9db72e07fcb7
kevin <kevinbex1@gmail.com>
2023-03-05 15:30:55 +0100

    added report and support button after len(data)==

--- cogs/search.py
+++ cogs/search.py
@@ -12,7 +12,7 @@ from functions.check_base_game import check_base_game
 from functions.get_steam_game import get_steam_game
 from functions.check_game_exists import check_game_exists
 from functions.check_game_in_db import check_game_in_db
-from functions.get_store import g2a,kinguin,k4g
+from functions.get_store import g2a, kinguin, k4g
 from functions.update_steamdb_game import update_steamdb_game
 from functions.check_steamlink import check_steamlink
 from helpers.db_connect import startsql as sql
@@ -23,7 +23,7 @@ import time
 from functions.normalized_text import normalized_text
 
 
-class G2A_button(discord.ui.Button):
+class G2AButton(discord.ui.Button):
     def __init__(self, label, style, custom_id, data):
         super().__init__(style=style, label=label, custom_id=custom_id)
         self.data = data
@@ -65,68 +65,38 @@ class G2A_button(discord.ui.Button):
             return None
 
 
-class DeleteEmbedView(discord.ui.View):
+class ButtonEmbed(discord.ui.View):
     def __init__(self, g2a_data, k4g_data, kinguin_data):
-        super(DeleteEmbedView, self).__init__()
+        super(ButtonEmbed, self).__init__(timeout=20)
         self.g2a = g2a_data
         self.k4g = k4g_data
         self.kinguin = kinguin_data
 
         if self.g2a:
-            self.add_item(G2A_button(label='G2A', style=discord.ButtonStyle.red, custom_id='g2a_button', data=g2a_data))
+            self.add_item(G2AButton(label='G2A', style=discord.ButtonStyle.red, custom_id='g2a_button', data=g2a_data))
         if self.k4g:
-            self.add_item(G2A_button(label='K4G', style=discord.ButtonStyle.red, custom_id='k4g_button', data=k4g_data))
+            self.add_item(G2AButton(label='K4G', style=discord.ButtonStyle.red, custom_id='k4g_button', data=k4g_data))
         if self.kinguin:
-            self.add_item(G2A_button(label='Kinguin', style=discord.ButtonStyle.red, custom_id='kinguin_button', data=kinguin_data))
+            self.add_item(G2AButton(label='Kinguin', style=discord.ButtonStyle.red, custom_id='kinguin_button',
+                                    data=kinguin_data))
 
-    # async def interaction_check(self, interaction: discord.Interaction):
-    #     if interaction.user.id == self.ctx.author.id:
-    #         return True
-    #     await interaction.response.send_message('You cannot use this.', ephemeral=True)
-    #     return False
 
-# class DeleteEmbedView(discord.ui.View):
-#     def __init__(self, g2a_data, k4g_data, kinguin_data):
-#         super(DeleteEmbedView, self).__init__()
-#         self.g2a = g2a_data
-#         self.k4g = k4g_data
-#         self.kinguin = kinguin_data
-#
-#     def list_to_embed(self, data, name):
-#         print(data, name)
-#         print(2)
-#         if data:
-#             prices_embed = discord.Embed(
-#                 title="Price information {}".format(name),
-#                 description=data[0][0],
-#                 color=0x9C84EF
-#             )
-#             if len(data) > 1:
-#                 for info in data:
-#                     prices_embed.add_field(
-#                         name="€{price}".format(price=info[3]),
-#                         value="[{name}]({url})".format(name=info[1], url=info[2])
-#                     )
-#             else:
-#                 prices_embed.add_field(
-#                     name="€{price}".format(price=data[0][3]),
-#                     value="[{name}]({url})".format(name=data[0][1], url=data[0][2])
-#                 )
-#             return prices_embed
-#         else:
-#             return None
-#
-#     @discord.ui.button(label='G2A', style=discord.ButtonStyle.red)
-#     async def g2a(self, interaction: discord.Interaction, button: discord.ui.Button):
-#         await interaction.response.send_message(embed=self.list_to_embed(self.g2a, "G2A"))
-#
-#     @discord.ui.button(label='K4G', style=discord.ButtonStyle.red)
-#     async def k4g(self, interaction: discord.Interaction, button: discord.ui.Button):
-#         await interaction.followup.send(embed=self.list_to_embed(self.k4g, "K4G"))
-#
-#     @discord.ui.button(label='Kinguin', style=discord.ButtonStyle.red)
-#     async def kinguin(self, interaction: discord.Interaction, button: discord.ui.Button):
-#         await interaction.followup.send(embed=self.list_to_embed(self.kinguin, "Kinguin"))
+class SupportButton(discord.ui.View):
+    def __init__(self, args):
+        super(SupportButton, self).__init__(timeout=20)
+        self.args = args
+
+    @discord.ui.button(label='Report', style=discord.ButtonStyle.green)
+    async def report(self, interaction: discord.Interaction, button: discord.ui.Button):
+        button.disabled = True
+        await interaction.response.edit_message(view=self)
+        await interaction.followup.send("I've reported this to support, head over to our support server for updates")
+
+    @discord.ui.button(label='Support', style=discord.ButtonStyle.blurple)
+    async def support(self, interaction: discord.Interaction, button: discord.ui.Button):
+        button.disabled = True
+        await interaction.response.edit_message(view=self)
+        await interaction.followup.send(">Support server link here<")
 
 
 class Search(commands.Cog, name="search"):
@@ -162,6 +132,7 @@ class Search(commands.Cog, name="search"):
                 return
             data = json.loads(byte_array)
             print(data)
+
         # data = check_game_exists(self.steam_apps, data)
 
         def get_game(args):
@@ -244,13 +215,13 @@ class Search(commands.Cog, name="search"):
                                "For DLC's, we need a Steam URL.")
             else:
                 if interaction is None:
-                    await ctx.send(embed=check_name, view=DeleteEmbedView(price_list_g2a,
-                                                                          price_list_k4g,
-                                                                          price_list_kinguin))
+                    await ctx.send(embed=check_name, view=ButtonEmbed(price_list_g2a,
+                                                                      price_list_k4g,
+                                                                      price_list_kinguin))
                 else:
-                    await interaction.followup.send(embed=check_name, view=DeleteEmbedView(price_list_g2a,
-                                                                                           price_list_k4g,
-                                                                                           price_list_kinguin))
+                    await interaction.followup.send(embed=check_name, view=ButtonEmbed(price_list_g2a,
+                                                                                       price_list_k4g,
+                                                                                       price_list_kinguin))
 
         # Search results 0
 
@@ -270,28 +241,23 @@ class Search(commands.Cog, name="search"):
             async def callback(interaction):
                 for choice in range(0, 11):
                     if select.values[0] == str(choice):
-                        # game_names = []
-                        # choice_data = normalized_text(data[choice-1]["name"].lower())
-                        # choice_alt_names = data[choice-1]["alternative_names"]
-                        # for alt_name in choice_alt_names:
-                        #     game_names.append(normalized_text(alt_name["name"].lower()))
-                        #
-                        # game_names.append(choice_data)
                         async with ctx.typing():
                             # Defer interaction earlier, so it does not expire before processing has finished
                             await interaction.response.defer()
-                            await print_game(data[choice-1], interaction)
+                            await print_game(data[choice - 1], interaction)
 
             select.callback = callback
             view = View()
             view.add_item(select)
             await ctx.send(embed=embed, view=view)
         # Search results one
-        else:
+        elif len(data) == 1:
             print(len(data), data)
             async with ctx.typing():
 
                 await print_game(data[0])
+        else:
+            await ctx.send(f"Couldn't find a game/dlc with the name `{args}`, if this is a mistake, please use a steam link or let us know about this", view=SupportButton(args))
 
 
 async def setup(bot):
