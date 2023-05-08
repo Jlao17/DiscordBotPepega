7062b43031e90f4daeafad6925cec36e4d6713a8
kevin <kevinbex1@gmail.com>
2023-05-08 21:24:27 +0200

    cleaning

--- functions/check_key_in_db.py
+++ functions/check_key_in_db.py
@@ -2,33 +2,27 @@ from helpers.db_connectv2 import startsql as sql
 
 
 async def check_key_in_db(id, shop):
-    print(4)
     if shop == "g2a":
-        print(5)
         search = await sql.fetchall("SELECT * FROM g2a WHERE ID = %s", id)
-        print(6)
-        print(search)
         if search:
-            print("Found keys in DB g2a")
+            print("Found keys in DB - G2A")
             return search
         if not search:
-            print("Found key not in DB g2a")
+            print("Found no key in DB - G2A")
             return None
     elif shop == "k4g":
         search = await sql.fetchall("SELECT * FROM k4g WHERE ID = %s", id)
-        print(5)
         if search:
-            print("Found keys in DB k4g")
+            print("Found keys in DB - K4G")
             return search
         if not search:
-            print("Found key not in DB k4g")
+            print("Found no key in DB - K4G")
             return None
     elif shop == "kinguin":
         search = await sql.fetchall("SELECT * FROM kinguin WHERE ID = %s", id)
-        print(5)
         if search:
-            print("Found keys in DB kinguin")
+            print("Found keys in DB - Kinguin")
             return search
         if not search:
-            print("Found key not in DB kinguin")
+            print("Found no key in DB - Kinguin")
             return None
--- functions/update_steamdb_game.py
+++ functions/update_steamdb_game.py
@@ -12,7 +12,6 @@ async def update_steamdb_game(game_data, result):
         price_final = game_data["price_overview"]["final"]
         price_discount = game_data["price_overview"]["discount_percent"]
     unix = int(time.time())
-    print(unix, price_initial, price_final, price_discount, game_data["type"])
     await sql.execute("UPDATE steamdb_test SET thumbnail = %s, price_initial = %s, price_final = %s, "
                       "discount = %s, type = %s, last_modified_search = %s WHERE steam_id = %s",
                       (game_data["header_image"], price_initial, price_final, price_discount, game_data["type"],
