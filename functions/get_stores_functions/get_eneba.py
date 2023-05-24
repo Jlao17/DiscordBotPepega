6b1f027f994e9f10399fb99b2158b0bf4b29a73b
Jlao17 <jasonlao1707@gmail.com>
2023-05-25 00:07:29 +0200

    simple filtering for eneba

--- functions/get_stores_functions/get_eneba.py
+++ functions/get_stores_functions/get_eneba.py
@@ -14,7 +14,11 @@ async def get_eneba(game_name, app_name, game_id, args, store):
     async def csv_parse(name, counter):
         df = pd.read_csv('eneba_csv.csv', skipinitialspace=True)
         df_dict = df.to_dict(orient='records')
+        log.info(df_dict)
         for game in df_dict:
+            if game['region'] != 'global':
+                continue
+
             offer_url = game['link']
             offer_price = game['price'].replace('EUR', '')
             offer_name = game['title']
