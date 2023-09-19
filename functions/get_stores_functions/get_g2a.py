f30d76c849a4705bbdea1b847b3c550c8b2502cc
kevin <kevinbex1@gmail.com>
2023-09-19 21:57:27 +0200

    test aiohttp request

--- functions/get_stores_functions/get_g2a.py
+++ functions/get_stores_functions/get_g2a.py
@@ -14,26 +14,29 @@ browser_headers = {
 
 async def get_g2a(game_name, app_name, game_id, args, store, user_cnf):
     async def json_request(name):
-        import requests
-
+        import aiohttp
         url = "https://www.g2a.com/search/api/v2/products"
 
-        querystring = {"itemsPerPage": "18",
-                       "include[0]": "filters",
-                       "currency": "EUR",
-                       "isWholesale": "false",
-                       "f[product-kind][0]": "10",
-                       "f[product-kind][1]": "8",
-                       "f[device][0]": "1118",
-                       "f[regions][0]": "8355",
-                       "category": "189",
-                       "phrase": name}
+        params = {
+            "itemsPerPage": "18",
+            "include[0]": "filters",
+            "currency": "EUR",
+            "isWholesale": "false",
+            "f[product-kind][0]": "10",
+            "f[product-kind][1]": "8",
+            "f[device][0]": "1118",
+            "f[regions][0]": "8355",
+            "category": "189",
+            "phrase": name
+        }
 
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/112.0",
         }
 
-        game_json = await requests.request("GET", url, headers=headers, params=querystring).json()
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, headers=headers, params=params) as response:
+                game_json = await response.json()
 
         return game_json
 
