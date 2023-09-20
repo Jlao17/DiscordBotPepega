2477842823749b009205504f6f707c738731fbf3
kevin <kevinbex1@gmail.com>
2023-09-20 22:38:20 +0200

    error testing

--- cogs/general.py
+++ cogs/general.py
@@ -259,7 +259,7 @@ class General(commands.Cog, name="general"):
         ssl_context = ssl.create_default_context(cafile=certifi.where())
 
         headers = {
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"
         }
         try:
             async with aiohttp.ClientSession() as session:
