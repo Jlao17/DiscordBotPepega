9fd8198a54facc806622e603afe7bc2222fb6d6b
Kevin Dev <kevinbex1@gmail.com>
2023-10-21 18:09:41 +0200

    steam update added test cache and improved updating based on steam_id

--- cogs/steam.py
+++ cogs/steam.py
@@ -19,7 +19,7 @@ class Steam(commands.Cog, name="steam"):
     def __init__(self, bot):
         self.bot = bot
         self.data = {}
-        self.data2 = {}
+        self.data_cache = {"cache_steamid": "0", "cache_steamid_test": "0"}
         self.cache = "0"
         self.key = ""
         self.browser_headers = {
@@ -28,21 +28,21 @@ class Steam(commands.Cog, name="steam"):
         self.getkey()
         # Comment this if you want to manually disable the steamdb check
         self.fillsteamdb.start()
-        self.get_eneba_csv.start()
+        #self.get_eneba_csv.start()
 
     def getkey(self):
         with open('cache.json') as json_file:
-            self.data = json.load(json_file)
-        self.cache = self.data["cache_steamid"]
+            self.data_cache = json.load(json_file)
+        self.cache = self.data_cache["cache_steamid"]
         with open('config.json') as json_fil:
             self.data = json.load(json_fil)
         self.key = self.data["steam_api"]
 
     async def write(self, change):
+        self.cache = f"{change}"
+        self.data_cache["cache_steamid"] = self.cache
         with open("cache.json", "w") as json_file:
-            self.cache = f"{change}"
-            self.data2["cache_steamid"] = self.cache
-            json.dump(self.data2, json_file)
+            json.dump(self.data_cache, json_file)
 
     @commands.hybrid_command(
         name="compare",
@@ -63,13 +63,15 @@ class Steam(commands.Cog, name="steam"):
         error = []
 
         async def get_store_apps(store_page, count):
+            error_count = 0
             start_time = time.time()
             for app in store_page:
-                if await sql.fetchone("SELECT * FROM steamdb_test WHERE NAME = %s", app["name"]) is None:
+                database_row = await sql.fetchone("SELECT * FROM steamdb WHERE steam_id = %s", app["appid"])
+                if database_row is None:
                     # await channel.send("**LOG** New game found: `{}`".format(app["name"]))
                     log.info("**LOG** New game found: `{}`".format(app["name"]))
-                    highest_row = await sql.fetchone("SELECT * FROM steamdb_test ORDER BY id DESC LIMIT 1")
-                    count = highest_row[0]
+                    highest_row = await sql.fetchone("SELECT * FROM steamdb ORDER BY id DESC LIMIT 1")
+                    count = highest_row[0] if highest_row is not None else 0
                     count += 1
                     game_appid = str(app["appid"])
                     game_name = str(app["name"])
@@ -81,22 +83,56 @@ class Steam(commands.Cog, name="steam"):
                     else:
                         try:
                             await sql.execute("INSERT INTO "
-                                              "steamdb_test (id, name, steam_id, last_modified, price_change_number) "
+                                              "steamdb (id, name, steam_id, last_modified, price_change_number) "
                                               "VALUES (%s, %s, %s, %s, %s)",
                                               (
                                                   str(count), game_name, game_appid, last_modified,
                                                   price_change_number))
                             await self.write(game_appid)
                         except:
+                            error_count += 1
                             error.append(game_appid)
                             traceback.print_exc()
                             if count % 5 == 0:
                                 await channel.send("**LOG** Failed inserting: {}".format(error))
                                 error.clear()
                             continue
+
+                # Check if it's the same -> ignore, if not -> update row
                 else:
-                    await self.write(str(app["appid"]))
-                    continue
+                    if database_row[1] == app["name"]:
+                        continue
+                    else:
+                        log.info("**LOG** Updating row in steamdb: `{}` to `{}`".format(database_row[1],app["name"]))
+                        count = database_row[0]
+                        game_appid = str(app["appid"])
+                        game_name = str(app["name"])
+                        last_modified = int(app["last_modified"])
+                        price_change_number = int(app["price_change_number"])
+
+                        if game_appid is None or game_name is None or last_modified is None:
+                            continue
+                        else:
+                            try:
+                                await sql.execute("UPDATE "
+                                                  "steamdb SET id = %s, name = %s, last_modified = %s, price_change_number = %s "
+                                                  "WHERE steam_id = %s",
+                                                  (
+                                                      str(count), game_name, last_modified,
+                                                      price_change_number, game_appid))
+                                await self.write(game_appid)
+                            except:
+                                error_count += 1
+                                error.append(game_appid)
+                                traceback.print_exc()
+                                if error_count % 5 == 0:
+                                    await channel.send("**LOG** Failed updating: {}".format(error))
+                                    error.clear()
+                                continue
+
+                # else:
+                #     await self.write(str(app["appid"]))
+                #     continue
             await channel.send(f"test done `{(time.time() - start_time) / 5000}` "
                                f"For the whole DB this will take "
                                f"`{(time.time() - start_time) / 5000}` x `119462` = "
@@ -112,7 +148,6 @@ class Steam(commands.Cog, name="steam"):
             headers=self.browser_headers
         ).json()
         try:
-            print(steam_apps_json["response"]["have_more_results"])
             while steam_apps_json["response"]["have_more_results"] is True:
                 steam_apps = steam_apps_json["response"]["apps"]
                 await get_store_apps(steam_apps, count)
@@ -149,6 +184,18 @@ class Steam(commands.Cog, name="steam"):
     async def before_get_eneba_csv(self):
         await self.bot.wait_until_ready()
         await asyncio.sleep(1)
+
+    async def gen_igdb_token(self):
+        url = 'https://id.twitch.tv/oauth2/token'
+        myobj = {'client_id': 'pdc47af0fviuz6lbxylft3jfdmb7kf',
+                 'client_secret': 'zuit09gobsu3f290avfz1mqu70xna1',
+                 'grant_type': 'client_credentials'
+                 }
+
+        x = requests.post(url, json=myobj)
+        print(x.text)
+
+
 
 async def setup(bot):
     await bot.add_cog(Steam(bot))
