d771a312d0f5ccfafebf8faa48206a252c21edc1
Jlao17 <jasonlao1707@gmail.com>
2023-11-14 21:16:15 +0100

    removed async get_tasks

--- cogs/g2a_update.py
+++ cogs/g2a_update.py
@@ -40,7 +40,7 @@ def remove_keywords(offer_name):
     return filtered_offer_name
 
 
-async def get_tasks(session, games):
+def get_tasks(session, games):
     tasks = []
     igdb_url = "https://api.igdb.com/v4/games"
     payload = "fields name, alternative_names.*, external_games.uid, external_games.category; limit 10; where " \
@@ -60,7 +60,7 @@ async def get_tasks(session, games):
 async def post_request(games):
     results = []
     async with aiohttp.ClientSession() as session:
-        retrieve_tasks = await get_tasks(session, games)
+        retrieve_tasks = get_tasks(session, games)
         responses = await asyncio.gather(*retrieve_tasks)
         for index, (response, game) in enumerate(zip(responses, games)):
             result = await response.json()
