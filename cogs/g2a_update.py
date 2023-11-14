da21bc7682c9e3b792b338756fed0f98aa0ee519
Jlao17 <jasonlao1707@gmail.com>
2023-11-14 23:51:26 +0100

    remove create_task

--- cogs/g2a_update.py
+++ cogs/g2a_update.py
@@ -50,29 +50,26 @@ async def get_tasks(session, games):
         'Client-ID': 'pdc47af0fviuz6lbxylft3jfdmb7kf',
     }
     for game in games:
-        tasks.append(asyncio.create_task(session.post(url=igdb_url,
-                                                      data=payload.format(game[0]),
-                                                      headers=headers, ssl=False)))
+        log.info("start appending task")
+        tasks.append(session.post(url=igdb_url,
+                                  data=payload.format(game[0]),
+                                  headers=headers, ssl=False))
+        log.info("task appended")
         await asyncio.sleep(1 / 4)
+    log.info("tasks created")
     return tasks
 
 
 async def post_request(games):
-    igdb_url = "https://api.igdb.com/v4/games"
-    payload = "fields name, alternative_names.*, external_games.uid, external_games.category; limit 10; where " \
-              "external_games.category = 1; search \"{}\"; "
-    headers = {
-        'Authorization': 'Bearer 9p8m4b9ydvlj2frabpysw1e6c6bbrr',
-        'Client-ID': 'pdc47af0fviuz6lbxylft3jfdmb7kf',
-    }
-
     results = []
     async with aiohttp.ClientSession() as session:
-        # retrieve_tasks = await get_tasks(session, games)
-        # responses = await asyncio.gather(*retrieve_tasks)
         sem = asyncio.Semaphore(1)
+        log.info("start retrieving tasks")
+        retrieve_tasks = await get_tasks(session, games)
+        log.info("start gathering tasks")
         async with sem:
-            responses = await asyncio.gather(*[session.post(url=igdb_url, data=payload.format(i[0]), headers=headers, ssl=False) for i in games])
+            responses = await asyncio.gather(*retrieve_tasks)
+        log.info("done gathering tasks")
         for index, (response, game) in enumerate(zip(responses, games)):
             result = await response.json()
 
@@ -81,20 +78,17 @@ async def post_request(games):
                 continue
             # Get the first steam_id from the result
             steam_id = None
-            try:
-                igdb_names = result[0]["external_games"]
-                for names in igdb_names:
-                    if names["category"] == 1:
-                        steam_id = names["uid"]
-                        break
+            igdb_names = result[0]["external_games"]
+            for names in igdb_names:
+                if names["category"] == 1:
+                    steam_id = names["uid"]
+                    break
 
-                # Add the steam_id to the corresponding game
-                if steam_id is not None:
-                    games[index] += (steam_id,)
+            # Add the steam_id to the corresponding game
+            if steam_id is not None:
+                games[index] += (steam_id,)
 
-                results.append(games[index])
-            except KeyError:
-                continue
+            results.append(games[index])
     return results
 
 
@@ -218,6 +212,7 @@ class G2aUpdate(commands.Cog, name="g2a_update"):
     async def before_fill_g2a_db(self):
         await self.bot.wait_until_ready()
         await asyncio.sleep(1)
+
 
 async def setup(bot):
     await bot.add_cog(G2aUpdate(bot))
