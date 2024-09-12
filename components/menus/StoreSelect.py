e2b15b07ed9b692e73f8b909a49c03db57cdaedc
Kevin Dev <kevinbex1@gmail.com>
2024-09-12 22:50:11 +0200

    fixed steam id showing instead of game name

--- components/menus/StoreSelect.py
+++ components/menus/StoreSelect.py
@@ -1,6 +1,7 @@
 import discord
 from components.menus.StoreSelectOption import StoreSelectOption
 from functions.list_to_embed import list_to_embed
+from functions.check_game_in_db import check_game_in_db
 import logging
 
 log = logging.getLogger(__name__)
@@ -29,6 +30,14 @@ class StoreSelect(discord.ui.Select):
         for option in self.options:  # Iterate over the options to find the selected option
             if option.value in selected_options:
                 selected_option_data = option.data  # Get the data associated with the selected option
+        log.info(selected_option_data)
+
+        # convert selected_option_data[0][0] to steam game name
+        game_name = await check_game_in_db(int(selected_option_data[0][0]))
+        temp_list = list(selected_option_data[0])
+        temp_list[0] = game_name[1]  # Modify the first element
+        selected_option_data[0] = tuple(temp_list)  # Convert it back to a tuple
+
         embed = await list_to_embed(selected_option_data, ''.join(selected_options), self.user_cnf)
         await interaction.response.defer()
         await interaction.message.edit(embed=embed, view=self.components)
